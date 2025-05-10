2ee4670b1df2cb0bf5d7a2eac7ab2e4a539e4f16
MATTEO LA MANNA <fortynumber>
2025-05-11 00:59:26 +0200

    Initial Commit

---
+++ TyreVibes/TyreVibesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TyreVibesApp.swift
+//  TyreVibes
+//
+//  Created by Matteo La Manna on 11/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TyreVibesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
